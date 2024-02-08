61b9aabdec8de1cddd409fec98af5c8352b2eeaf
GuerraSalasIvan <ivan.guerra.salas.al@iespoligonosur.org>
2024-02-08 14:47:18 +0100

    carpeta

---
+++ angular17/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CarruselComponent } from './carrusel/carrusel.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CarruselComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
