55859ad1e1d1369032b734abf585780926b082c7
GuerraSalasIvan <guerrasalasivan@gmail.com>
2024-02-14 21:47:36 +0000

    carrusel

--- angular17/src/app/carrusel/carrusel.component.ts
+++ angular17/src/app/carrusel/carrusel.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { PeticionService } from '../peticion.service';
 
 @Component({
   selector: 'app-carrusel',
   templateUrl: './carrusel.component.html',
-  styleUrl: './carrusel.component.scss'
+  styleUrls: ['./carrusel.component.scss']
 })
-export class CarruselComponent {
+export class CarruselComponent implements OnInit {
+  peliculas: any[] = [];
 
+  constructor(private http: HttpClient, private peticionService: PeticionService) {}
+
+  ngOnInit(): void {
+    this.peticionService.getPopularMovies().subscribe(data => {
+      this.peliculas = data.results;
+    });
+  }
 }
--- angular17/src/app/peticion.service.ts
+++ angular17/src/app/peticion.service.ts
@@ -6,16 +6,14 @@ import { HttpClient } from '@angular/common/http';
 })
 export class PeticionService {
 
-  apiUrlTopRated = 'https://api.themoviedb.org/3/movie/top_rated'
+  apiUrl = 'https://api.themoviedb.org/3';
   apiKey = '665eddc29536d1ffc4e5fdace47ae8c7';
 
   constructor(private http: HttpClient) { }
 
-    getTopRated(){
-      const url = `${this.apiUrlTopRated}&api_key=${this.apiKey}`;
-      return this.http.get(url)
-    }
-      
-       
+  getPopularMovies() {
+    const url = `${this.apiUrl}/movie/popular?api_key=${this.apiKey}`;
+    return this.http.get<any>(url);
+  }
 }
 
